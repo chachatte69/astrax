c11e415df2754921545dd8541a1c757559030997
chachatte69 <113788575+chachatte69@users.noreply.github.com>
2022-10-24 18:21:59 +0200

    Add files via upload

---
+++ astrax/js/cookies.js
@@ -0,0 +1,7 @@
+localStorage.setItem("musiques", "20");
+localStorage.removeItem("musiques");
+
+sessionStorage.setItem("watchtime", "500")
+sessionStorage.removeItem("watchtime")
+
+document.cookie = `astrax=cookies; expires=${new StageChannel(2022, 11, 17).toUTCString()}`
